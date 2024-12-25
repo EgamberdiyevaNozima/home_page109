f5af4b978b375088e5c185ed2703f3dbc09fd033
EgamberdiyevaNozima <sohibjonovna03@gmail.com>
2024-12-25 20:45:20 +0500

    9-vazifa uyga

---
+++ lib/utils/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color backgroundColor = Color(0xFF1E1E1E);
+  static const Color textColor = Color(0xFFFFFFFF);
+  static const Color whiteColor = Color(0xFFFFFFFF);
+  static const Color pinkColor = Color(0xFFFF4081);
+}
---
+++ lib/utils/app_images.dart
@@ -0,0 +1,11 @@
+class AppImages {
+  static const String im1 = 'assets/images/image1.png';
+  static const String im2 = 'assets/images/image2.png';
+  static const String im3 = 'assets/images/image3.png';
+  static const String ch1 = 'assets/images/ch1.png';
+  static const String ch2 = 'assets/images/ch2.png';
+  static const String ch3 = 'assets/images/ch3.png';
+  static const String ch4 = 'assets/images/ch4.png';
+  static const String tos = 'assets/images/tos.png';
+  static const String lim = 'assets/images/lim.png';
+}
